50ba41e204db1696ce6cdf07daa1bce9fd052840
fernandafons <nanda.fonseca7@gmail.com>
2020-11-08 19:48:24 -0300

    login working

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user.mode';
+import { ToastController, LoadingController, NavController } from '@ionic/angular';
+import { AngularFireAuth } from '@angular/fire/auth';
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  user = {} as User;
 
-  constructor() { }
+  constructor(
+    private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController,
+    private afAuth: AngularFireAuth,
+    private navCtrl: NavController
+  ) { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  async login(user: User) {
+    if (this.formValidation()) {
+      //show loader
+      let loader = this.loadingCtrl.create({
+        message: "Please wait..."
+      });
+      (await loader).present();
+
+      try {
+        await this.afAuth
+            .signInWithEmailAndPassword(user.email, user.password)
+            .then(data => {
+              console.log(data);
+
+              // redirect to homepage
+              this.navCtrl.navigateRoot("home");
+            });
+      } catch (e) {
+        this.showToast(e);
+      }
+
+      //dismiss loader
+      (await loader).dismiss();
+    }
+  }
+
+  formValidation() {
+    if (!this.user.email){
+      this.showToast("Enter email");
+      return false;
+    }
+
+    if (!this.user.password){
+      this.showToast("Enter password");
+      return false;
+    }
+
+    return true;
+  }
+
+  showToast(message: string) {
+    this.toastCtrl.create({
+      message: message,
+      duration: 3000
+    }).then(toastData => toastData.present());
   }
 
 }
